b4099b6e61b1cc0647fe225704483fa2a3139200
IlaMuniz <ilammuniz@gmail.com>
2024-07-06 21:29:07 -0300

    Criação do projeto e inicialização dos dados do cliente

---
+++ src/Conta.java
@@ -0,0 +1,13 @@
+public class Conta {
+    public static void main(String[] args) {
+        String nome = "Arthur Conan Doyle";
+        String tipoConta = "Corrente";
+        double saldo = 5699.90;
+
+        System.out.println("*****************\n");
+        System.out.println("Nome do cliente: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n*****************");
+    }
+}
